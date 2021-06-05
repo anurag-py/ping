0bf98bb39134d2b40388dd34ec1d82028c346e74
anurag-py <anurag.hhs@gmail.com>
2021-06-05 18:52:01 +0530

    flask new

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+import requests
+import subprocess
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    flag = True
+    while(flag):
+        response = requests.get("http://punlaonline.herokuapp.com")
+        print(response.status_code)
+        if response.status_code == 200:
+            flag=False
+            break
+    subprocess.call(["python","ping.py"], shell=False)
+    
+
+    return "Flag is %s " % flag
--- ping.py
+++ ping.py
@@ -1,10 +1,12 @@
 import requests
+import time
 flag = True
 while(flag):
     response = requests.get("http://punlaonline.herokuapp.com")
     print(response.status_code)
-    if response.status_code == 200:
-        flag=False
-        break
+    time.sleep(60)
+    # if response.status_code == 200:
+    #     flag=False
+    #     break
     
 
