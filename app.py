87dfd1fddc5d4b113a5ea2fd86fc833a075d08eb
anurag-py <anurag.hhs@gmail.com>
2021-06-07 13:14:39 +0530

    added ap scheduler

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask
 import requests
-import subprocess
+# import subprocess
+from flask_apscheduler import APScheduler
 
+
+# set configuration values
+class Config:
+    SCHEDULER_API_ENABLED = True
 
 app = Flask(__name__)
+
+
+# initialize scheduler
+scheduler = APScheduler()
+# if you don't wanna use a config, you can set options here:
+# scheduler.api_enabled = True
+# scheduler.init_app(app)
+
+# cron examples
+@scheduler.task('cron', id='ping', minute='*/25')
+def job2():
+    response = requests.get("http://punlaonline.herokuapp.com")
+    print(response.status_code)
+    print('Job 2 executed')
+
+scheduler.start()
 
 @app.route("/")
 def index():
@@ -14,10 +35,13 @@ def index():
         if response.status_code == 200:
             flag=False
             break
-    subprocess.call(["python","ping.py"], shell=False)
+    
     
 
     return "Flag is %s " % flag
 
-@app.route("/app-running")
-    return "App is runnning"
+# @app.route("/app-running")
+# def page():
+#     subprocess.call(["python","ping.py"], shell=False)
+#     return "App is runnning"
+
